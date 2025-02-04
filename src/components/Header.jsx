2dd87dad19738bb01ce6a66d59112b34474b836a
DieHard073055 <eshanshafeeq073055@gmail.com>
2025-02-05 02:01:41 +0500

    fix: logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import logo from "/src/assets/logo.webp";
 
 const Header = () => (
   <header className="bg-light-yellow p-4 text-center">
-    <img src="/assets/logo.webp" alt="Logo" className="mx-auto" />
+    <img src={logo} alt="Logo" className="mx-auto" />
     <h1 className="text-4xl font-bold text-dark-teal">Tropical Treasures</h1>
     <nav className="flex justify-center mt-2">
       <a href="/" className="mx-4 text-dark-teal hover:text-coral-red">
